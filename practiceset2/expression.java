bb8220d208160f736d89dbe0df81b926b8fdb054
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-07-08 18:32:06 +0530

    this is a practice_set

---
+++ practiceset2/expression.java
@@ -0,0 +1,9 @@
+package Practics_set_repo_java.practiceset2;
+
+public class expression {
+    public static void main(String[] args) {
+        float result= 7/4f * 9/2f;
+        System.out.println(result);
+    }
+    
+}
---
+++ practiceset2/name.java
@@ -0,0 +1,41 @@
+package Practics_set_repo_java.practiceset2;
+
+public class name {
+
+    public static void main(String[] args) {
+        char name= 'A';
+        // print 'P'
+        name=(char)(name + 15);
+        System.out.print(name);
+
+        //print 'R'
+        name =(char)(name + 2);
+        System.out.print(name);
+
+        //print 'A'
+        name =(char)(name - 17);
+        System.out.print(name);
+
+        //print 'S'
+        name =(char)(name + 18);
+        System.out.print(name);
+
+        //print 'H'
+        name =(char)(name - 11);
+        System.out.print(name);
+
+        //print 'A'
+        name =(char)(name - 7);
+        System.out.print(name);
+
+        //print 'N'
+        name =(char)(name + 13);
+        System.out.print(name);
+
+        //print 'T'
+        name =(char)(name +6);
+        System.out.println(name);
+
+
+    }
+}
