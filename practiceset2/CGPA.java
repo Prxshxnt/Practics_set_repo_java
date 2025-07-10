3b178363459028cfc5d4372821a9d5acddfb85ef
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-07-10 15:18:16 +0530

    RESULT

---
+++ practiceset2/CGPA.java
@@ -0,0 +1,19 @@
+package Practics_set_repo_java.practiceset2;
+
+import java.util.Scanner;
+
+public class CGPA {
+    public static void main(String[] args) {
+        Scanner CGPA=new Scanner(System.in);
+        System.out.println("Enter the of DBMS: ");
+        int DBMS=CGPA.nextInt();
+        System.out.println("Enter the marks of DAA: ");
+        int DAA=CGPA.nextInt();
+        System.out.println("Enter the marks of Big Data: ");
+        int Big_Data=CGPA.nextInt();
+        float result= ((DBMS+DAA+Big_Data))/30f;
+        System.out.println("The CGPA is: " +result );
+        
+    }
+    
+}
