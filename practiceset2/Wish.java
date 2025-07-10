9cf76a2f78bdaf83c110adc9c0420197ace72c51
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-07-10 15:30:12 +0530

    wish

---
+++ practiceset2/Wish.java
@@ -0,0 +1,13 @@
+package Practics_set_repo_java.practiceset2;
+import java.util.Scanner;
+
+public class Wish {
+    public static void main(String[] args) {
+        Scanner ask=new Scanner(System.in);
+        System.out.println("Hii Enter you good name: ");
+        String name=ask.nextLine();
+        System.out.println(" Hello " + name + "!" + "," +" have a good day ");
+
+    }
+    
+}
