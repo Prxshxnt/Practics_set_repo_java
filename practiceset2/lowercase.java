84869b2cb70f88912dff1ac248ee917b346cfb65
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-07-11 22:24:33 +0530

    escape

--- practiceset2/lowercase.java
+++ practiceset2/lowercase.java
@@ -8,6 +8,13 @@ public class lowercase {
         String name=con.nextLine();
         String lowstr=name.toLowerCase();
         System.out.println(lowstr);
+
+        // this code for escape sequence char
+        String letters= "Hello Dear ,\n\t This code is write by Prashant.\n \t Java is a programming language";
+        System.out.println(letters);
+
+        String lette= "Hello Dear ,\t This code is write by Prashant.\t Java is a programming language";
+        System.out.println(lette);
     }
     
 }
