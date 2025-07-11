aa923cbc1ba6cc4f47cff633415c5951ab9c9b43
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-07-11 18:32:20 +0530

    one more ex add

--- practiceset2/replace.java
+++ practiceset2/replace.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class replace {
     public static void main(String[] args) {
         Scanner rep=new Scanner(System.in);
+        System.out.println("Enter your text here: ");
         String text=rep.nextLine();
         text=text.replace(" ", "_");
         System.out.println(text);
+
+        //One another Example
+        System.out.println("Here the one another example: ");
+        String letter = "Dear <|name|>, thanks a lot.";
+        letter = letter.replace("<|name|>", "Prashant");
+        System.out.println(letter);
         
     }
     
