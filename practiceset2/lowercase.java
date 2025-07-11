67a57558709a341046122da67ba29dc61bafb997
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-07-11 15:47:20 +0530

    lowercase

---
+++ practiceset2/lowercase.java
@@ -0,0 +1,13 @@
+package Practics_set_repo_java.practiceset2;
+
+import java.util.Scanner;
+
+public class lowercase {
+    public static void main(String[] args) {
+        Scanner con=new Scanner(System.in);
+        String name=con.nextLine();
+        String lowstr=name.toLowerCase();
+        System.out.println(lowstr);
+    }
+    
+}
