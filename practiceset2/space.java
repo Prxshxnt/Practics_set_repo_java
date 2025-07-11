435c9d56356634b9ca5b61fbd207a80e9ccd69f4
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-07-11 21:45:36 +0530

    space

---
+++ practiceset2/space.java
@@ -0,0 +1,10 @@
+package Practics_set_repo_java.practiceset2;
+
+public class space {
+    public static void main(String[] args) {
+    String text="Hello sir how can i help  you ";
+    System.out.println(text.indexOf("  "));
+    System.out.println(text.indexOf("   "));
+    }
+    
+}
