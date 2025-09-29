0b2d025732249e0eaa8afd6691895c418ba25b0f
Prxshxnt <prashant.chauhan902k@gmail.com>
2025-09-29 21:55:57 +0530

    solid rho... update

--- StarPattern/solidroh.java
+++ StarPattern/solidroh.java
@@ -2,9 +2,15 @@ package Practics_set_repo_java.StarPattern;
 
 public class solidroh {
     public static void main(String[] args) {
-        int n=4;
+        int n=5;
         for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=5 ; j++){
+
+            for(int j=1 ; j<=n-i ; j++)
+            {
+                System.out.print("   ");
+            }
+            for(int j=1 ; j<=n ; j++)
+            {
                 System.out.print(" * ");
             }
             System.out.println();
